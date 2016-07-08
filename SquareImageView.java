e602a1084be66ef6fc074dc7bdddb6febcba892f
张利高 <zhangligao999@users.noreply.github.com>
2016-07-08 16:10:48 +0800

    Create SquareImageView.java

---
+++ SquareImageView.java
@@ -0,0 +1,28 @@
+package com.zlg.rewriteViews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by zlg on 2016/7/1.
+ */
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
